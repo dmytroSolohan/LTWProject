f5ff4bed7991b2d803bedcf65fcdf7a58045fbfe
drakons06 <dmytro.solohan@gmail.com>
2020-04-25 15:43:13 +0200

    new site style

--- js/login.js
+++ js/login.js
@@ -2,12 +2,3 @@
 function verificaLogin(){
     return false;
 };
-
-function showPSW() {
-    var x = document.getElementById("psw");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  };
--- js/setups.js
+++ js/setups.js
@@ -1,7 +1,45 @@
-    /*menu*/
+    /*nascondi mostra menu*/
 $(document).ready(function(){
     $("#sidebarCollapse").click(function(e){
       e.preventDefault();
       $("#wrapper").toggleClass("menuDisplayed");
     });
   });
+
+  /**spostamento davanti col dietro*/
+  var $accMsg = $('.acc-Msg'),
+  $accedi = $('.accedi'),
+  $regMsg = $('.reg-Msg'),
+  $registrati = $('.registrati'),
+  $davanti = $('.davanti');
+
+$('#puls-sin').on('click', function() {
+  $accMsg.toggleClass("visibility");
+  $davanti.addClass("moving");
+  $regMsg.toggleClass("visibility");
+
+  $('#puls-sin').attr("disabled", true);
+  $('#puls-des').attr("disabled", false);
+
+  $registrati.toggleClass('hide');
+  $accedi.toggleClass('hide');
+});
+
+$('#puls-des').on('click', function() {
+  $accMsg.toggleClass("visibility");
+  $davanti.removeClass("moving");
+  $regMsg.toggleClass("visibility");
+
+  $('#puls-des').attr("disabled", true);
+  $('#puls-sin').attr("disabled", false);
+
+  $registrati.toggleClass('hide');
+  $accedi.toggleClass('hide');
+});
+
+  /* psw lettura nel campo password da hover *O*  */
+  $('#checkpsw').hover(function () {
+    $('#psw').attr('type', 'text');
+}, function () {
+    $('#psw').attr('type', 'password');
+});
