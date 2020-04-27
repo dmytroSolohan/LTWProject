23f74aac96ca56c983b285bb6e862424504ea20d
drakons06 <dmytro.solohan@gmail.com>
2020-04-27 21:39:07 +0200

    added php and ajax request

--- js/login.js
+++ js/login.js
@@ -31,7 +31,7 @@
     $registrati.toggleClass('hide');
     $accedi.toggleClass('hide');
   });
-  });
+
   
     /* psw lettura nel campo password da hover *O*  */
     $('#checkpsw').hover(function () {
@@ -45,6 +45,7 @@
     if(input.trim().match(/^([a-zA-Z0-9_\-\.]+)\s([a-zA-Z0-9_\-\.]+)$/) == null) {
             return false;
     }
+
     return true;
     }
     /* valida mail*/
@@ -54,9 +55,10 @@
         }
         return true;
     }
+    
     /* semplice password vuota o min 8 caratteri*/
     function validatePass(input){
-        if(input.trim() == '' || input.trim().length < 8){
+        if(input.trim().length < 8){
             return false;
         }
         return true;
@@ -70,41 +72,84 @@
         var password = $('#psw-l').val();
         
         if(!validateMail(mail)){
-            $('.invalid-mail-l').toggleClass('hide');
-            $('.email-l').toggleClass('margine');
+            $('.invalid-mail-l').removeClass('hide');
+            $('.email-l').removeClass('margine');
+        }
+        else {
+            $('.invalid-mail-l').addClass('hide');
+            $('.email-l').addClass('margine');
         }
             
         if(!validatePass(password)){
-            $('.invalid-psw-l').toggleClass('hide');
-            $('.password-l').toggleClass('margine');
+            $('.invalid-psw-l').removeClass('hide');
+            $('.password-l').removeClass('margine');
         }
-            
+        else {
+            $('.invalid-psw-l').addClass('hide');
+            $('.password-l').addClass('margine');
+        }
+            /*   qui codice per acccedere */
+
+                //prove
+        if(validateMail(mail) && validatePass(password)){
+            var ajaxRequest =$.ajax({
+                    type:'POST',
+                    url:'server/checklogin.php',
+                    dataType:'json',
+                    data:{mail:mail, password:password},
+                    success: function(data) {
+                        if (!data.success) { 
+                            if (data.errors.name) { 
+                                $('.throw_error').fadeIn(1000).html(data.errors.name); 
+                            }
+                        }
+                        else {
+                            //sessionStorage.setItem({mail:mail, id: '1111'}, JSON.stringify(data));
+                            $(location).attr('href',"server/login.html");
+                            }
+                        }
+            });
+
+        }
     });
 
-$(document).ready(function(){
+
     $('#registra').submit(function(event){
         event.preventDefault();
 
         var nome = $('#nome-r').val();
-        var mail = $('#email-r').val();
-        var psw = $('#psw-r').val();
+        var email = $('#email-r').val();
+        var passw = $('#pasw-r').val();
         
         if(!validateName(nome)){
-            $('.invalid-nome-r').toggleClass('hide');
-            $('.nome-l').toggleClass('margine');
+            $('.invalid-nome-r').removeClass('hide');
+            $('.nome-r').removeClass('margine');
+        }
+        else {
+            $('.invalid-nome-r').addClass('hide');
+            $('.nome-r').addClass('margine');
         }
 
-        if(!validateMail(mail)){
-            $('.invalid-mail-r').toggleClass('hide');
-            $('.email-l').toggleClass('margine');
+        if(!validateMail(email)){
+            $('.invalid-mail-r').removeClass('hide');
+            $('.email-r').removeClass('margine');
+        }
+        else {
+            $('.invalid-mail-r').addClass('hide');
+            $('.email-r').addClass('margine');
+        }
+                    
+        if(!validatePass(passw)){
+            $('.invalid-psw-r').removeClass('hide');
+            $('.pasw-r').removeClass('margine');
+        }
+        else {
+            $('.invalid-psw-r').addClass('hide');
+            $('.pasw-r').addClass('margine');
         }
             
-        if(!validatePass(psw)){
-            $('.invalid-psw-r').toggleClass('hide');
-            $('.password-l').toggleClass('margine');
-        }
-            
+        /*   qui codice per registrarsi */
+
     });
-});
 
-    
+});
--- server/checklogin.php
+++ server/checklogin.php
@@ -1,3 +1,40 @@
 <?php
+    /*
+    $name = $_POST['mail'];
+    $psw = $_POST['password'];
 
+    $arr = array(
+        "Email" => "ciccio@ciccio.it",
+        "Password" => "1234qwer"
+    );
+
+    echo json_encode($array); */
+
+    //collegarsi al db
+    //fare la query
+    //cercare persona per email
+    //vedere se la password è corretta
+    //mandare boolean della risposta
+
+    //prove per vedere come funziona
+    $errors = array(); //To store errors
+    $form_data = array(); //Pass back the data to `form.php`
+
+    /* Validate the form on the server side */
+    if ($_POST['email' == 'ciccio@ciccio.it'] && $_POST['password' == '1234qwer']) { 
+        $errors['email'] = 'ciccio@ciccio.it';
+        $errors['passwrod'] = '1234qwer';
+    }
+
+    if (!empty($errors)) { //If errors in validation
+        $form_data['success'] = false;
+        $form_data['errors']  = $errors;
+    }
+    else { //If not, process the form, and return true on success
+        $form_data['success'] = true;
+        $form_data['posted'] = 'Data Was Posted Successfully';
+    }
+
+    //Return the data back to form.php
+    echo json_encode($_POST);
 ?>
