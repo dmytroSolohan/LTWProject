6f8f3c3a993931179007fc0c7351ea4f8256ed44
drakons06 <dmytro.solohan@gmail.com>
2020-05-02 13:28:24 +0200

    parte cerca quando loggato funziona

--- server/querydb.php
+++ server/querydb.php
@@ -3,12 +3,7 @@
     //prove per vedere come funziona
     $errors = array('db' => false); //To store errors
     $form_data = array(); //Pass back the data 
-    $materia = array();
-    $prof = array();
-    $aula = array(); 
-    $data = array(); 
-    $voto = array(); 
-    $descrizione = array();
+    $data = array();
 
 /* Validate the form on the server side */
     //if(isset($_POST['log-in'])){
@@ -30,19 +25,21 @@
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
             while($riga = mysqli_fetch_assoc($result)){
+                $row = array();
+                array_push($row, $riga['materia']);
+                array_push($row, $riga['nome_prof']);
+                array_push($row, $riga['aula']);
+                array_push($row, $riga['data']);
+                array_push($row, $riga['voto']);
+                array_push($row, $riga['descrizione']);
 
-                array_push($materia, $riga['materia']);
-                array_push($prof, $riga['nome_prof']);
-                array_push($aula, $riga['aula']);
-                array_push($data, $riga['data']);
-                array_push($voto, $riga['voto']);
-                array_push($descrizione, $riga['descrizione']);
+                array_push($data, $row);
 
             }
             
             $form_data['success'] = true;
             $form_data['posted'] = 'Success !';
-            $form_data['data'] = array($materia, $prof, $aula, $data, $voto, $descrizione);
+            $form_data['data'] = $data;
 
         }
         mysqli_stmt_close($stmt);
