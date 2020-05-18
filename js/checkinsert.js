e334a15418a787c4a42d28d51561ce7b09bc6d21
drakons06 <dmytro.solohan@gmail.com>
2020-05-18 16:28:17 +0200

    aggiunto check inserimento dati form user

--- js/checkinsert.js
+++ js/checkinsert.js
@@ -1,14 +1,26 @@
-$(document).ready(function(){
 
-    function validateName(input){
-        if(input.trim().match(/^([a-zA-Z0-9_\-\.\s]+)$/) == null) {
-                return false;
-        }
+$(document).ready(function () {
+
+    function validateName(input) {
+        if (input.trim().match(/^([a-zA-Z0-9_\-\.\s]+)$/) == null)
+            return false;
         return true;
     }
-    
+
+    function validateVoto(input) {
+        if (input.trim() != null || isNaN(input.trim()))
+            return true;
+        return false;
+    }
+
+    function validateDate(input) {
+        if (input.trim().match(/^(19[5-9][0-9]|20[0-4][0-9]|2050)-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/) == null)
+            return false;
+        return true;
+    }
+
     /* */
-    $(document).on("submit" , "#sub-data", function(event){
+    $(document).on("submit", "#sub-data", function (event) {
         event.preventDefault();
 
         var nome_prof = $('#nome_prof').val();
@@ -17,35 +29,28 @@ $(document).ready(function(){
         var aula = $('#aula').val();
         var date = $('#date').val();
         var descrizione = $('#descrizione').val();
-        
-        console.log(nome_prof);
-        console.log(materia);
-        console.log(voto);
-        console.log(aula);
-        console.log(date);
-        console.log(descrizione);
 
-        if(validateName(nome_prof) && validateName(materia)){
-            var ajaxRequest =$.ajax({
-                    type:'POST',
-                    url:'./updatevotes.php',
-                    dataType:'json',
-                    data:{nome_prof:nome_prof, materia:materia, voto:voto, aula:aula, date:date, descrizione:descrizione, id:sessionStorage.getItem("userId")}
+        if (validateName(nome_prof) && validateName(materia) && validateVoto(voto) && validateName(aula) && validateDate(date) && validateName(descrizione)) {
+            var ajaxRequest = $.ajax({
+                type: 'POST',
+                url: './updatevotes.php',
+                dataType: 'json',
+                data: { nome_prof: nome_prof, materia: materia, voto: voto, aula: aula, date: date, descrizione: descrizione, id: sessionStorage.getItem("userId") }
             });
-            
-            ajaxRequest.done(function(return_data){
-                if(return_data.success){
+
+            ajaxRequest.done(function (return_data) {
+                if (return_data.success) {
                     alert("Dati inseriti correttamente");
                 }
-                else{
+                else {
                     alert(return_data.posted);
                 }
             });
-            ajaxRequest.fail(function(return_data){
+            ajaxRequest.fail(function (return_data) {
                 alert("Errore con il server, riprovare!");
             });
         }
-        else{
+        else {
             alert("Dati non corretti!");
         }
 
--- js/checkpersonalinfo.js
+++ js/checkpersonalinfo.js
@@ -1,5 +1,31 @@
 
-$(document).on("submit" , "#modifica-dati", function(event){
+$(document).ready(function () {
+
+    function validateName(input) {
+        if (input.trim().match(/^([a-zA-Z\s]+)$/) == null)
+            return false;
+        return true;
+    }
+
+    function validateIndirizzo(input) {
+        if (input.trim().match(/^([a-zA-Z0-9,.\s]+)$/) == null)
+            return false;
+        return true;
+    }
+
+    function validateDate(input) {
+        if (input.trim().match(/^(19[5-9][0-9]|20[0-4][0-9]|2050)-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/) == null)
+            return false;
+        return true;
+    }
+
+    function validateCell(input) {
+        if (input.trim().match(/^\+?(39)?([\d+]{9,10})$/) == null)
+            return false;
+        return true;
+    }
+
+    $(document).on("submit", "#modifica-dati", function (event) {
         event.preventDefault();
 
         var ID = sessionStorage.getItem("userId")
@@ -10,24 +36,31 @@ $(document).on("submit" , "#modifica-dati", function(event){
         var citta = $('#citta').val();
         var paese = $('#paese').val();
         var telefono = $('#telefono').val();
-        
-        var ajaxRequest =$.ajax({
-                type:'POST',
-                url:'./updatepersonalinfo.php',
-                dataType:'json',
-                data:{ID:ID, nome:nome, cognome:cognome, data_nascita:data_nascita, indirizzo:indirizzo, citta:citta, paese:paese, telefono:telefono}
-        });
-            
-        ajaxRequest.done(function(return_data){
-            if(return_data.success){
-                alert("Dati modificati correttamente");
-            }
-            else{
-                alert(return_data.posted);
-            }
-        });
-        ajaxRequest.fail(function(return_data){
-            alert("Errore con il server, riprovare!");
-        });
-            
+
+        if (validateName(nome) && validateName(cognome) && validateDate(data_nascita) && validateIndirizzo(indirizzo) && validateName(citta) && validateName(paese) && validateCell(telefono)) {
+
+            var ajaxRequest = $.ajax({
+                type: 'POST',
+                url: './updatepersonalinfo.php',
+                dataType: 'json',
+                data: { id: ID, nome: nome, cognome: cognome, data_nascita: data_nascita, indirizzo: indirizzo, citta: citta, paese: paese, telefono: telefono }
+            });
+
+            ajaxRequest.done(function (return_data) {
+                if (return_data.success) {
+                    alert("Dati modificati correttamente");
+                }
+                else {
+                    alert(return_data.posted);
+                }
+            });
+            ajaxRequest.fail(function (return_data) {
+                alert("Errore con il server, riprovare!");
+            });
+        }
+        else {
+            alert("Dati non corretti!");
+        }
+
     });
+});
